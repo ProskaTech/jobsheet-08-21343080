5c8bd6e0d6815f1c301e03883dfe370a9da60721
21343080 <112677254+21343080@users.noreply.github.com>
2022-10-13 18:57:33 +0700

    Add files via upload

---
+++ src/ArraySample2.java
@@ -0,0 +1,12 @@
+/*
+ * Created by_21343080 Andi Sultan Al Ayyubi
+ */
+
+public class ArraySample2 {
+    public static void main (String [] args){
+        int [] ages = new int [100];
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print (ages[i]);
+        }
+    }
+}
